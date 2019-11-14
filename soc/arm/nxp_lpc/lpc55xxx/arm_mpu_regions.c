9780b5f6391f0694b81e7369bd46e4c4dafd9f81
Andrei Gansari <andrei.gansari@nxp.com>
2019-11-14 15:34:53 +0200

    soc: lpc55xxx mpu regions add SRAMX
    Code bus mapped memory SRAMX added to LPC55xxx's MPU.
    
    Signed-off-by: Andrei Gansari <andrei.gansari@nxp.com>

--- soc/arm/nxp_lpc/lpc55xxx/arm_mpu_regions.c
+++ soc/arm/nxp_lpc/lpc55xxx/arm_mpu_regions.c
@@ -18,6 +18,11 @@ static const struct arm_mpu_region mpu_regions[] = {
 		CONFIG_SRAM_BASE_ADDRESS,
 		REGION_RAM_ATTR(CONFIG_SRAM_BASE_ADDRESS, \
 			CONFIG_SRAM_SIZE * 1024)),
+	/* Region 2 */
+	MPU_REGION_ENTRY("SRAM_X",
+		DT_INST_5_MMIO_SRAM_BASE_ADDRESS,
+		REGION_RAM_ATTR(DT_INST_5_MMIO_SRAM_BASE_ADDRESS, \
+			DT_INST_5_MMIO_SRAM_SIZE * 1024)),
 };
 
 const struct arm_mpu_config mpu_config = {
