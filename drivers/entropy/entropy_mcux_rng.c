e0a8b41acbc448bd17f37a4f4c2aaaba3301af54
Andrei Gansari <andrei.gansari@nxp.com>
2020-03-19 17:59:30 +0200

    lpc entropy
    Signed-off-by: Andrei Gansari <andrei.gansari@nxp.com>

---
+++ drivers/entropy/entropy_mcux_rng.c
@@ -0,0 +1,54 @@
+/*
+ * Copyright (c) 2017 Linaro Limited.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+#include <device.h>
+#include <drivers/entropy.h>
+#include <random/rand32.h>
+#include <init.h>
+
+#include "fsl_rng.h"
+
+struct mcux_entropy_config {
+	RNG_Type *base;
+};
+
+static int entropy_mcux_rng_get_entropy(struct device *dev, u8_t *buffer,
+					 u16_t length)
+{
+	const struct mcux_entropy_config *config = dev->config->config_info;
+	status_t status;
+
+	ARG_UNUSED(dev);
+
+	status = RNG_GetRandomData(config->base, buffer, length);
+	__ASSERT_NO_MSG(!status);
+
+	return 0;
+}
+
+static const struct entropy_driver_api entropy_mcux_rng_api_funcs = {
+	.get_entropy = entropy_mcux_rng_get_entropy
+};
+
+static struct mcux_entropy_config entropy_mcux_config = {
+	.base = (RNG_Type *)DT_INST_0_NXP_LPC_RNG_BASE_ADDRESS
+};
+
+static int entropy_mcux_rng_init(struct device *);
+
+DEVICE_AND_API_INIT(entropy_mcux_rng, DT_INST_0_NXP_LPC_RNG_LABEL,
+		    entropy_mcux_rng_init, NULL, &entropy_mcux_config,
+		    PRE_KERNEL_1, CONFIG_KERNEL_INIT_PRIORITY_DEVICE,
+		    &entropy_mcux_rng_api_funcs);
+
+static int entropy_mcux_rng_init(struct device *dev)
+{
+	ARG_UNUSED(dev);
+
+	RNG_Init(entropy_mcux_config.base);
+
+	return 0;
+}
